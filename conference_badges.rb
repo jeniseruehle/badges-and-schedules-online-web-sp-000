4bbba11fca58dec20e00d1ae503217bf6121be58
Jenise Ruehle <jela07@gmail.com>
2020-01-29 17:30:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,7 @@ def badge_maker(name)
 end   
 
 def batch_badge_creator(attendees)
-  attendees = []
-  attendees.each do |name|
-    badge_maker(name)
+    attendees.each do |name|
+    return badge_maker(name)
   end
 end   
