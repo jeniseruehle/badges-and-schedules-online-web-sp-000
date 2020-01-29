9528cc36e11e97bf9515d0dbb8d194d333bc54ef
Jenise Ruehle <jela07@gmail.com>
2020-01-29 17:51:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,7 @@ def batch_badge_creator(attendees)
 end   
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |name, index| 
+  attendees.each_with_index.map do |name, index| 
     "Hello, #{name}! You\'ll be assigned to room #{index + 1}!"
   end  
-  return list
 end   
